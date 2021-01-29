5240a25df701c0a816bb61510c195dc8a67271bd
Mitchell Walker <eagleye1144@gmail.com>
2021-01-29 09:31:20 -0700

    Task 1 and 2 Complete

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+//This is Function Level Scope. When you declare a variable inside of a function you can't access it out of it, but you can within the function.
+//Since "nestedFunction" is within "myFunction", we are allowed to use "internal".
 
 
 
@@ -28,12 +29,16 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
+function summation(number) {
   /*Your Code Here*/
-
+  let counter = 0;
+  for (let i=1; i <= number; i++){
+    counter += i;
+  }
+    return counter;
   }
  
-
+console.log(summation(4));
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
 
